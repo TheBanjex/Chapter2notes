6a8ae4ff9e57b8a27c0f8a3e07791735c056d964
f66k <f66k@fresnou.org>
2023-02-15 09:38:06 -0800

    initial commit

---
+++ input1.js
@@ -0,0 +1,12 @@
+//1
+const readLine = require('readline-sync');
+
+let answer = readLine.question("wanna hear a joke\nyour answer:")
+console.log(answer);
+
+if (answer == "yes");
+console.log('Police arrested two kids yesterday, one was drinking battery acid, the other was eating fireworks. They charged one – and let the other one off');
+
+let respond = readLine.question("was it funny?\nyour answer:")
+console.log(answer);
+if (answer == "yes");
