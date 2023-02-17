cb0e7311c43395af4f9ce312008c77e8d5e8f8fe
f66k <f66k@fresnou.org>
2023-02-17 10:27:24 -0800

    initial commit

--- input1.js
+++ input1.js
@@ -1,12 +1,12 @@
 //1
 const readLine = require('readline-sync');
 
-let answer = readLine.question("wanna hear a joke\nyour answer:")
+let choice = readLine.question("wanna hear a joke\nyour answer:")
 console.log(answer);
 
-if (answer == "yes");
+if (choice == "yes");
 console.log('Police arrested two kids yesterday, one was drinking battery acid, the other was eating fireworks. They charged one – and let the other one off');
 
 let respond = readLine.question("was it funny?\nyour answer:")
 console.log(answer);
-if (answer == "yes");
+if (choice == "yes");
